8b0d4afb938007576bf5dfb062eeac027a037328
dimitrov570 <ivajlo.dimitrov99@gmail.com>
2020-05-20 01:12:34 +0300

    fix output when no file is found

--- scripts/05-b-7200.sh
+++ scripts/05-b-7200.sh
@@ -9,7 +9,13 @@ if [ -f "${arg}" -a -r "${arg}" ]; then
 elif [ -d "${arg}" -a -r "${arg}" -a -x "${arg}" ]; then
 	nrOfFiles=$(find "${arg}" -mindepth 1 -type f 2> /dev/null | wc -l)
 	echo "Files in '${arg}' with size greater than ${nrOfFiles} bytes:";
-	find "${arg}" -mindepth 1 -size -${nrOfFiles}c -printf "\t%p\n" 2> /dev/null
+	result="$(find "${arg}" -mindepth 1 -size -${nrOfFiles}c -printf "\t%p\n" 2> /dev/null)"
+	if [ -n "${result}" ]; then
+			echo "${result}";
+	else
+			echo -e "\t-No files found that match that criteria!";
+	fi
+	
 else
 	echo "'${arg}' can't be accessed!" >&2;
 fi
