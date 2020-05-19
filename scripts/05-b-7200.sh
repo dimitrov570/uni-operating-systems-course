7c99cd771e20ace371ab7a76b9e4d13f8c28b9f3
dimitrov570 <ivajlo.dimitrov99@gmail.com>
2020-05-20 01:07:26 +0300

    add new task

--- scripts/05-b-7200.sh
+++ scripts/05-b-7200.sh
@@ -5,12 +5,12 @@
 for arg; do
 
 if [ -f "${arg}" -a -r "${arg}" ]; then
-	echo "File ${arg} can be read!";
+	echo "File '${arg}' is readable!";
 elif [ -d "${arg}" -a -r "${arg}" -a -x "${arg}" ]; then
 	nrOfFiles=$(find "${arg}" -mindepth 1 -type f 2> /dev/null | wc -l)
-	echo "${arg}:";
+	echo "Files in '${arg}' with size greater than ${nrOfFiles} bytes:";
 	find "${arg}" -mindepth 1 -size -${nrOfFiles}c -printf "\t%p\n" 2> /dev/null
 else
-	echo "${arg} can't be accessed!" >&2;
+	echo "'${arg}' can't be accessed!" >&2;
 fi
 done
