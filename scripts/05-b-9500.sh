47a121f506aa90b247b63f7f52127659c1d1e692
dimitrov570 <ivajlo.dimitrov99@gmail.com>
2020-05-21 14:20:04 +0300

    add new task

---
+++ scripts/05-b-9500.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -eq 0 -o $# -gt 2 ]; then
+		echo "Wrong number of parameters!" >&2
+		exit 1
+fi
+
+color=
+
+if [ $# -eq 2 ]; then
+	case "${1}" in
+		"-r")	color="\033[0;31m"
+				;;
+		"-g")	color="\033[0;32m"
+				;;
+		"-b")	color="\033[0;34m"
+				;;
+		   *) 	echo "Unknown colour!"	
+			  	exit 2
+				;;
+	esac
+	echo -e "${color}${2}";
+else
+	echo "${1}";
+fi
+
+
+
