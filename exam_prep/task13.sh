f57a58517d20d4727c885cb7894a3c84516e4420
dimitrov570 <ivajlo.dimitrov99@gmail.com>
2020-06-26 20:40:58 +0300

    add new task

---
+++ exam_prep/task13.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+		exit 1;
+fi
+
+if [ ! -d $1 ]; then
+		exit 2;
+fi
+
+find "$1" -type l -printf "%Y %p\n" 2> /dev/null | awk '{if ($1 == "N" || $1 == "L" || $1 == "?")print $2}'
